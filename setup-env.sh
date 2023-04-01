8937b4a847225b68476ffddd984390103c280f6a
shelooks16 <blauyedz@zoho.eu>
2023-04-01 23:27:39 +0100

    added setup env script

---
+++ setup-env.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+functions_env="apps/functions/env/.runtimeconfig.json"
+service_account="service-account.dev.json"
+
+echo "=== Setting up environment files for the project"
+
+if [ ! -e $functions_env ]
+then
+  firebase functions:config:get > $functions_env
+  echo "$functions_env => Runtime config pulled for functions"
+fi
+
+if [ ! -e $service_account ]
+then
+  echo -n "" > $service_account
+  echo "$service_account => Created empty file. Download service account from Google Cloud and put inside"
+fi
+
+echo "=== Complete"
