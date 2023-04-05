a5a08f5fff46c5d7c33675eeb08ac8a07fdf2259
shelooks16 <blauyedz@zoho.eu>
2023-04-05 17:20:55 +0100

    consider usecases for old tz names. manually transform tz name to supported one by nodejs

--- packages/dates/timezone.ts
+++ packages/dates/timezone.ts
@@ -1,8 +1,18 @@
+const normalizeTzName = (tzName: string) => {
+  if (tzName.includes('Kyiv')) {
+    return 'Europe/Kiev';
+  }
+
+  return tzName;
+};
+
 /**
  * Get offset in minutes for a timezone
  */
 export const getTimezoneOffset = (timeZone: string, date = new Date()) => {
   const utcDate = new Date(date.toLocaleString('en-US', { timeZone: 'UTC' }));
-  const tzDate = new Date(date.toLocaleString('en-US', { timeZone }));
+  const tzDate = new Date(
+    date.toLocaleString('en-US', { timeZone: normalizeTzName(timeZone) })
+  );
   return (tzDate.getTime() - utcDate.getTime()) / 6e4;
 };
