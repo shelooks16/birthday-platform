04fde87e87e60ac447abbf341be0b43ec2b3bac8
shelooks16 <blauyedz@zoho.eu>
2023-04-26 18:45:48 +0100

    fix doc match path

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -7,22 +7,22 @@ export * from './telegramBot.types';
 
 export const FireCollection = {
   profiles: {
-    docMatch: 'profile/${id}',
+    docMatch: 'profile/{id}',
     path: () => 'profile',
     docPath: (id: string) => `profile/${id}`
   },
   birthdays: {
-    docMatch: 'birthday/${id}',
+    docMatch: 'birthday/{id}',
     path: () => 'birthday',
     docPath: (id: string) => `birthday/${id}`
   },
   notifications: {
-    docMatch: 'notification/${id}',
+    docMatch: 'notification/{id}',
     path: () => 'notification',
     docPath: (id: string) => `notification/${id}`
   },
   emailVerification: {
-    docMatch: 'emailVerification/${id}',
+    docMatch: 'emailVerification/{id}',
     path: () => 'emailVerification',
     docPath: (id: string) => `emailVerification/${id}`
   }
