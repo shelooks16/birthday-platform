a2622786b4ebaf84cd78c73bf9fc91295255b441
shelooks16 <blauyedz@zoho.eu>
2023-05-13 02:31:23 +0100

    use english british locale instead of us

--- packages/locales/index.ts
+++ packages/locales/index.ts
@@ -3,18 +3,18 @@ import en from './en';
 import ru from './ru';
 import uk from './uk';
 
-const localeMap = { en, ru, uk };
+const localeMap = { 'en-GB': en, ru, uk };
 
 export type SupportedLocale = keyof typeof localeMap;
 
-export const fallbackLocale: SupportedLocale = 'en';
+export const fallbackLocale: SupportedLocale = 'en-GB';
 export const localeList: {
   locale: SupportedLocale;
   label: string;
 }[] = [
   { locale: 'uk', label: 'Українська' },
   { locale: 'ru', label: 'Русский' },
-  { locale: 'en', label: 'English' }
+  { locale: 'en-GB', label: 'English' }
 ];
 
 export const loadDictionary = (locale: SupportedLocale) => localeMap[locale];
